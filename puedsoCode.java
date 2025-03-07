ac8c7c92ae54d05e14234cae07addd163684a084
EmilyM <155667566+EmilyMenken@user.noreply.github.com>
2025-03-07 12:05:58 -0800

    puesdoCode

---
+++ puedsoCode.java
@@ -0,0 +1,30 @@
+public class puedsoCode {
+    
+
+
+
+
+
+    // between n and m
+// int n = m
+// 
+
+
+// count words between n and m characters in an array
+
+
+
+// create count 0
+
+// loop through the array
+    // each word, find count, length
+    //if length is > n and < m
+        // increase count
+
+// return count
+
+
+
+
+
+}
